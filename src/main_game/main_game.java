9b69a8dbb5973c047d3b2ad9b03bf8a6a6882a9a
ssuyu1220 <69281366+ssuyu1220@users.noreply.github.com>
2024-05-26 00:17:42 +0800

    Update main_game.java

--- src/main_game/main_game.java
+++ src/main_game/main_game.java
@@ -19,146 +19,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.stage.Stage;
 
-class PausePage extends Pane {
-
-    private Group backgroundGroup = new Group();
-    private Stage stage = new Stage();
-    private BorderPane startpane = new BorderPane();
-    private BorderPane contigamepane = new BorderPane();
-    private BorderPane newgamPane = new BorderPane();
-    private eelsAndEscalator origin;
-    private Scene origionscene;
-
-    File restartfile = new File("resources/restart.jpg");
-    Image restartImage = new Image(restartfile.toURI().toString());
-    ImageView restartImageView = new ImageView(restartImage);
-
-    File homefile = new File("resources/home.jpg");
-    Image homeImage = new Image(homefile.toURI().toString());
-    ImageView homeImageView = new ImageView(homeImage);
-
-    File continuefile = new File("resources/continue.jpg");
-    Image continueImage = new Image(continuefile.toURI().toString());
-    ImageView continueImageView = new ImageView(continueImage);
-
-    public PausePage(Stage stage, BorderPane pane, eelsAndEscalator originEelsAndEscalator, Scene origionScene) {
-        this.stage = stage;
-        startpane = pane;
-        // 原本正在進行的遊戲
-        contigamepane.setCenter(originEelsAndEscalator);
-        // 新的遊戲
-        eelsAndEscalator newEelsAndEscalator = new eelsAndEscalator();
-        newgamPane.setCenter(newEelsAndEscalator);
-        // 指派本來在進行的遊戲給origin
-        origin = originEelsAndEscalator;
-        origionscene = origionScene;
-        getChildren().addAll(backgroundGroup, restartImageView, homeImageView, continueImageView);
-        Pause();
-    }
-
-    public void Pause() {
-
-        double hie = getHeight();
-        double wid = getWidth();
-
-        restartImageView.setLayoutX(wid * 0.37);
-        restartImageView.setLayoutY(hie * 0.45);
-        restartImageView.setFitWidth(wid * 0.05);
-        restartImageView.setFitHeight(hie * 0.05);
-
-        homeImageView.setLayoutX(wid * 0.47);
-        homeImageView.setLayoutY(hie * 0.45);
-        homeImageView.setFitWidth(wid * 0.05);
-        homeImageView.setFitHeight(hie * 0.05);
-
-        continueImageView.setLayoutX(wid * 0.57);
-        continueImageView.setLayoutY(hie * 0.45);
-        continueImageView.setFitWidth(wid * 0.05);
-        continueImageView.setFitHeight(hie * 0.05);
-
-        Rectangle backgroundRec = new Rectangle();
-        backgroundRec.setWidth(wid * 2);
-        backgroundRec.setHeight(hie * 2.2);
-        backgroundRec.setFill(Color.WHITE);
-        backgroundRec.setStroke(Color.BLACK);
-        backgroundRec.setLayoutX(0);
-        backgroundRec.setLayoutY(0);
-        backgroundGroup.getChildren().add(backgroundRec);
-
-        restartImageView.setOnMouseClicked(re -> {
-            Button restarButton = new Button("PAUSE");
-            restarButton.styleProperty().bind(Bindings.concat(
-                    "-fx-font-size: ", contigamepane.heightProperty().divide(30).asString(), "px; ",
-                    "-fx-text-fill: black; ",
-                    "-fx-font-family: 'Times New Roman'; ",
-                    "-fx-font-weight: bold; ",
-                    "-fx-font-style: italic;"));
-
-            newgamPane.setTop(restarButton);
-            BorderPane.setAlignment(restarButton, Pos.CENTER);
-            Scene newgamescene = new Scene(newgamPane, 1200, 700);
-            stage.setScene(newgamescene);
-
-            restarButton.setOnMouseClicked(cl -> {
-                BorderPane PausePane = new BorderPane();
-                PausePage pausepage = new PausePage(stage, new BorderPane(startpane), origin, origionscene);
-                PausePane.setCenter(pausepage);
-                Scene pausesScene = new Scene(PausePane, 1200, 700);
-                stage.setScene(pausesScene);
-
-            });
-
-            Scene newScene = new Scene(newgamPane, 1200, 700);
-            stage.setScene(newScene);
-        });
-
-        homeImageView.setOnMouseClicked(ho -> {
-            Scene homeScene = new Scene(startpane, 1200, 700);
-            stage.setScene(homeScene);
-        });
-
-        continueImageView.setOnMouseClicked(co -> {
-
-            Button restarButton = new Button("PAUSE");
-            restarButton.styleProperty().bind(Bindings.concat(
-                    "-fx-font-size: ", contigamepane.heightProperty().divide(30).asString(), "px; ",
-                    "-fx-text-fill: black; ",
-                    "-fx-font-family: 'Times New Roman'; ",
-                    "-fx-font-weight: bold; ",
-                    "-fx-font-style: italic;"));
-
-            contigamepane.setTop(restarButton);
-            BorderPane.setAlignment(restarButton, Pos.CENTER);
-
-            stage.setScene(origionscene);
-
-            restarButton.setOnMouseClicked(cl -> {
-                BorderPane PausePane = new BorderPane();
-                PausePage pausepage = new PausePage(stage, new BorderPane(startpane), origin, origionscene);
-                PausePane.setCenter(pausepage);
-                Scene pausesScene = new Scene(PausePane, 1200, 700);
-                stage.setScene(pausesScene);
-
-            });
-
-            Scene cotinueScene = new Scene(contigamepane, 1200, 700);
-            stage.setScene(cotinueScene);
-        });
-    }
-
-    public void setWidth(double width) {
-        super.setWidth(width);
-        Pause();
-    }
-
-    @Override
-    public void setHeight(double height) {
-        super.setHeight(height);
-        Pause();
-    }
-
-}
-
 class eelsAndEscalator extends Pane {
 
     private Group escalator = new Group();
@@ -172,8 +32,6 @@ class eelsAndEscalator extends Pane {
     public eelsAndEscalator() {
         getChildren().addAll(baGroup, gameImageView, ButtonGroup);
         getChildren().addAll(steps.steps);
-        getChildren().addAll(steps.ladders);
-        getChildren().add(dice_handler.moving);
         PlayEelsAndEscalator();
 
         // Bind the width and height properties to ensure dynamic resizing
@@ -219,7 +77,6 @@ class eelsAndEscalator extends Pane {
 }
 
 public class main_game extends Application {
-
     public void start(Stage arg0) {
         BorderPane Pane = new BorderPane();
         String StartGame = "CLICK TO START THE GAME";
@@ -244,7 +101,6 @@ public class main_game extends Application {
         Scene scene = new Scene(Pane, 1200, 700);
         Pane.setOnMouseClicked(ev -> {
             steps.creat_steps();
-            steps.create_ladders();
             BorderPane gamePane = new BorderPane();
             eelsAndEscalator game = new eelsAndEscalator();
             gamePane.setCenter(game);
@@ -257,12 +113,9 @@ public class main_game extends Application {
                     "-fx-font-family: 'Times New Roman'; ",
                     "-fx-font-weight: bold; ",
                     "-fx-font-style: italic;"));
-
             top_box.getChildren().addAll(restarButton, steps.dice);
             top_box.setAlignment(Pos.CENTER);
             gamePane.setTop(top_box);
-
-            gamePane.setTop(restarButton);
             BorderPane.setAlignment(restarButton, Pos.CENTER);
             Scene gamescene = new Scene(gamePane, 1200, 700);
             arg0.setScene(gamescene);
