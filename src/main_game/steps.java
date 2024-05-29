a2c70af06e44d46dd1fd2fac57b6f25502943341
huan9 <99493310+1syuju@users.noreply.github.com>
2024-05-29 21:21:40 +0800

    Update steps.java

--- src/main_game/steps.java
+++ src/main_game/steps.java
@@ -5,9 +5,7 @@ import javafx.animation.PauseTransition;
 import javafx.animation.RotateTransition;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
-import javafx.beans.Observable;
 import javafx.collections.ObservableList;
-import javafx.collections.ObservableListBase;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -16,10 +14,14 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Polyline;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -36,10 +38,11 @@ import three_coloring_puzzle_game.*;
 class dice_handler implements EventHandler<ActionEvent> {
     private int lucky = 0;
     ///
-    private int once=0;
-    private int climb=0;
+    private int once = 0;
+    private int climb = 0;
     private int slide=0;
-    public static Circle moving=new Circle(6.0);
+    public static Circle moving = new Circle(6.0);
+    public static int winner;
     @Override
     public void handle(ActionEvent e) {
         tmp_dice.result.setText("");
@@ -49,50 +52,48 @@ class dice_handler implements EventHandler<ActionEvent> {
 
             @Override
             public void handle(ActionEvent e) {
-                ///
+                System.out.println("cur"+steps.cur_player);
                 once++;
-                if(once<=1){
+                RotateTransition rotateTransition = new RotateTransition(Duration.seconds(2), tmp_dice.roulette);
+                rotateTransition.setFromAngle(0);// restart from 0
+                if (once <= 1) {
                     lucky = (int) (Math.random() * 12) + 1;
-                    RotateTransition rotateTransition = new RotateTransition(Duration.seconds(2), tmp_dice.roulette);
-                    rotateTransition.setFromAngle(0);// restart from 0
                     rotateTransition.setByAngle(360 + 30 * (12 - lucky)); // rotate 360 + 30 * (12 - lucky)
                     rotateTransition.setAutoReverse(false); // 不自動反轉
                     // start to rotate
                     rotateTransition.play();
                     // display result text
                     String roll = new String();
-                    lucky = lucky % 6 + 1;
-                    if (lucky == 1){
-                        roll = "go " + Integer.toString(lucky) + " step";
+                    lucky = lucky % 6+1;    
+                    System.out.println("lucky"+lucky);
+                    if (lucky == 1) {
+                        roll = "go " + 1 + " step";
+                    } else if (lucky == 5 ) {
+                        roll = "go " + 2 + " steps";
                     }
-                    else if (lucky < 4){
-                        roll = "go " + Integer.toString(lucky) + " steps";
+                    else if(lucky == 3){
+                        roll = "go " + 3 + " steps";
                     }
-                    else if(lucky == 5){
-                        /*
-                         * 我先設轉到梯子並在梯子前一格才能爬梯子
-                         * 如果不符合條件則重轉
-                         */
-                        switch(steps.players_pos[steps.cur_player]){
+                    else if (lucky == 2) {
+                        switch (steps.players_pos[steps.cur_player]) {
                             case 0:
-                                roll ="climb ladder";
-                                climb=1;
+                                roll = "climb ladder";
+                                climb = 1;
                                 break;
                             case 10:
-                                roll ="climb ladder";
-                                climb=3;
+                                roll = "climb ladder";
+                                climb = 3;
                                 break;
                             case 4:
-                                roll ="climb ladder";
-                                climb=2;
+                                roll = "climb ladder";
+                                climb = 2;
                                 break;
                             default:
-                                once=0;
-                                roll ="spin again";
+                                once = 0;
+                                roll = "spin again";
                                 break;
                         }
-                    }
-                    else if(lucky == 6){
+                    } else if (lucky == 6) {
                         switch(steps.players_pos[steps.cur_player]){
                             case 4:
                                 roll ="eel";
@@ -111,23 +112,21 @@ class dice_handler implements EventHandler<ActionEvent> {
                                 slide=4;
                                 break;
                             default:
-                                once=0;
+                                once = 0;
                                 roll ="spin again";
                                 break;
                         }
-                    }
-                    else{
+                    } else {
                         roll = "play secreat game";
-                        int secretGame=(int) (Math.random() * 3) + 1;
-                        //paulse for 3 sec.
-                        PauseTransition delay = new PauseTransition(Duration.seconds(3));
-                        delay.setOnFinished(event ->{
-                            Stage stage=new Stage();
-                            if (secretGame==1){
+                        int secretGame = (int) (Math.random() * 3) + 1;
+                        // paulse for 3 sec.
+                        PauseTransition delay = new PauseTransition(Duration.seconds(2));
+                        delay.setOnFinished(event -> {
+                            Stage stage = new Stage();
+                            if (secretGame == 1) {
                                 new guessnumber().start(stage);
-                            }
-                            else if(secretGame==2){
-                                new Main().start(stage);
+                            } else if (secretGame == 2) {
+                                new three_coloring_puzzle_game().start(stage);
                             }
                             stage.show();
                         });
@@ -143,47 +142,52 @@ class dice_handler implements EventHandler<ActionEvent> {
         tmp_dice.dice_stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
             public void handle(WindowEvent e) {
-                if(lucky==5){
-                    int aim=0;
-                    Color color=steps.player_color[steps.cur_player];
+                if (lucky == 2) {
+                    int aim = 0;
+                    System.out.println(steps.players_pos[steps.cur_player]+"climb"+climb);
+                    Color color = steps.player_color[steps.cur_player];
                     moving.setFill(color);
                     moving.setVisible(true);
                     TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(2), moving);
-                    switch(climb){
-                        case 1: //1-10
-                            aim=10;
+                    switch (climb) {
+                        case 1: // 1-10
+                            aim = 10;
                             translateTransition.setFromX(390.0);
                             translateTransition.setFromY(495.0);
                             translateTransition.setToX(525.0);
                             translateTransition.setToY(340.0);
+                            steps.sliding();
                             translateTransition.play();
                             break;
-                        case 2: //5-8
-                            aim=8;
-                            translateTransition.setFromX(390.0);
+                        case 2: // 5-8
+                            aim = 8;
+                            translateTransition.setFromX(900.0);
                             translateTransition.setFromY(495.0);
                             translateTransition.setToX(760.0);
                             translateTransition.setToY(345.0);
+                            steps.sliding();
                             translateTransition.play();
                             break;
-                        case 3: //11-14
-                            aim=14;
+                        case 3: // 11-14
+                            aim = 14;
                             translateTransition.setFromX(400.0);
                             translateTransition.setFromY(340.0);
                             translateTransition.setToX(260.0);
                             translateTransition.setToY(190.0);
+                            steps.sliding();
                             translateTransition.play();
                             break;
                     }
-                    translateTransition.setCycleCount(1); 
+                    translateTransition.setCycleCount(1);
                     translateTransition.setAutoReverse(false);
-                    steps.players_pos[steps.cur_player]=aim;
-                    translateTransition.setOnFinished(e1->{
-                        steps.cur_player = (steps.cur_player + 1) % 4;
+                    steps.players_pos[steps.cur_player] = aim;
+                    translateTransition.setOnFinished(e1 -> {
                         steps.set_pos_visible();
                         moving.setVisible(false);
+                        steps.win();
+                        steps.cur_player = (steps.cur_player + 1) % 4;
                     });
-                }
+                } 
                 else if(lucky==6){
                     Color color=steps.player_color[steps.cur_player];
                     switch(slide){
@@ -201,6 +205,7 @@ class dice_handler implements EventHandler<ActionEvent> {
                             ani1.setOnFinished(e4->{
                                 moving.setVisible(false);
                                 steps.players_pos[steps.cur_player]=0;
+                                steps.win();
                                 steps.set_pos_visible();
                                 steps.cur_player = (steps.cur_player + 1) % 4;
                             });
@@ -218,6 +223,7 @@ class dice_handler implements EventHandler<ActionEvent> {
                             ani2.setOnFinished(e4->{
                                 moving.setVisible(false);
                                 steps.players_pos[steps.cur_player]=0;
+                                steps.win();
                                 steps.set_pos_visible();
                                 steps.cur_player = (steps.cur_player + 1) % 4;
                             });
@@ -235,6 +241,7 @@ class dice_handler implements EventHandler<ActionEvent> {
                             ani3.setOnFinished(e4->{
                                 moving.setVisible(false);
                                 steps.players_pos[steps.cur_player]=0;
+                                steps.win();
                                 steps.set_pos_visible();
                                 steps.cur_player = (steps.cur_player + 1) % 4;
                             });
@@ -251,6 +258,7 @@ class dice_handler implements EventHandler<ActionEvent> {
                             steps.sliding();
                             ani4.setOnFinished(e4->{
                                 steps.players_pos[steps.cur_player]=3;
+                                steps.win();
                                 steps.set_pos_visible();
                                 moving.setVisible(false);
                                 steps.cur_player = (steps.cur_player + 1) % 4;
@@ -258,13 +266,20 @@ class dice_handler implements EventHandler<ActionEvent> {
                             break;
                     }
                 }
-                else{
-                    steps.players_pos[steps.cur_player] += lucky;
-                    steps.cur_player = (steps.cur_player + 1) % 4;
+                else if(lucky==5){
+                    steps.players_pos[steps.cur_player] += 2;
+                    steps.win();
                     steps.set_pos_visible();
+                    steps.cur_player = (steps.cur_player + 1) % 4;
                 }
-                lucky=0;
-                once=0;
+                else {
+                    steps.players_pos[steps.cur_player] += lucky;
+                    steps.win();
+                    steps.set_pos_visible();
+                    steps.cur_player = (steps.cur_player + 1) % 4;
+                }
+                lucky = 0;
+                once = 0;
             }
         });
     }
@@ -272,8 +287,7 @@ class dice_handler implements EventHandler<ActionEvent> {
 
 public class steps {
     public static Group[] steps = new Group[20];
-    public static Group ladders=new Group(); //ladders
-    public static ObservableList<Double> eelsList0;
+    public static Group ladders = new Group(); // ladders
     public static Circle[][] player_sym = new Circle[20][4];
     public static Button dice = new Button("dice");
     public static int cur_player = 0;
@@ -281,11 +295,11 @@ public class steps {
     public static Color[] player_color = { Color.DARKSLATEGRAY, Color.DARKCYAN, Color.DARKGREY, Color.DARKMAGENTA };
     public static Polyline[] eel=new Polyline[4]; //eel
     public static Group[] eels=new Group[4];
-    public static void create_ladders(){
-        Rectangle[] basic=new Rectangle[3];
-        for(int i=0;i<3;i++){
-            basic[i]=new Rectangle(70,200,Color.TRANSPARENT);
-            //basic[i].setStroke(Color.WHEAT);
+    public static void create_ladders() {
+        Rectangle[] basic = new Rectangle[3];
+        for (int i = 0; i < 3; i++) {
+            basic[i] = new Rectangle(70, 200, Color.TRANSPARENT);
+            // basic[i].setStroke(Color.WHEAT);
         }
         basic[0].setLayoutX(365.0);
         basic[0].setLayoutY(190.0);
@@ -370,7 +384,7 @@ public class steps {
             }
             steps[i] = new Group();
             back[i] = new Rectangle(70, 30, Color.GOLD);
-            steps[i].getChildren().addAll(back[i], players_box[i],new Label(Integer.toString(i)));
+            steps[i].getChildren().addAll(back[i], players_box[i], new Label(Integer.toString(i)));
         }
         steps[0].setLayoutX(265.0);
         steps[0].setLayoutY(495.0);
@@ -437,5 +451,43 @@ public class steps {
     }
     public static void sliding() {
         player_sym[players_pos[cur_player]][cur_player].setVisible(false);
+    }
+    public static void game_data_init() {
+        cur_player = 0;
+        players_pos[0] = 0;
+        players_pos[1] = 0;
+        players_pos[2] = 0;
+        players_pos[3] = 0;
+
+    }
+    public static void win(){
+        Font fnt2 = Font.font("Consolas", FontWeight.BOLD, FontPosture.REGULAR, 48);
+        Label winlb=new Label("WIN!!!\nWIN!!!");
+        Label sgtlb=new Label("click to restart");
+        Button clickbtn=new Button("click");
+        BorderPane winPane=new BorderPane();
+        if(players_pos[cur_player]>=19){
+            players_pos[cur_player]=19;
+            player_sym[19][cur_player].setVisible(true);
+            Stage pop=new Stage();
+            winlb.setFont(fnt2);
+            winPane.setCenter(winlb);
+            VBox vb=new VBox();
+            vb.getChildren().addAll(sgtlb,clickbtn);
+            vb.setAlignment(Pos.CENTER);
+            winPane.setBottom(vb);
+            Scene winScene=new Scene(winPane,500,300);
+            pop.setScene(winScene);
+            pop.show();
+            clickbtn.setOnAction(e->{
+                pop.close();
+                main_game.main_stage.setScene(main_game.gamescene);
+                game_data_init();
+                set_pos_visible();
+            });
+        }
+        else{
+            return;
+        }
     }
 }
